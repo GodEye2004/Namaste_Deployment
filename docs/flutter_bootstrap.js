ba5aeed3a7632db8187acf45daad36705034e6e9
Mohammad Mahdi <111296448+GodEye2004@users.noreply.github.com>
2025-06-07 14:23:24 +0330

    new

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2239367738"
+    serviceWorkerVersion: "1202374065"
   }
 });
